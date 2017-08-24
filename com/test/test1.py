0c5461795d553ae6413cfd2233bb8f38dfbba29d
JQ0324 <542722627@qq.com>
2017-08-24 09:40:52 +0800

    add udp

--- com/test/test1.py
+++ com/test/test1.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+import socket  
 
 class Fid:
     def __init__(self,n):
@@ -27,12 +29,38 @@ def fun(n):
         n -= 1
         yield cur
         prev ,cur = cur, cur+prev
+        
+def read_file(fpath):
+    BLOCK_SIZE = 1024
+    with open (fpath,'rb') as f:
+        while True:
+            block = f.read(BLOCK_SIZE)
+            if block:
+                yield block
+            else:
+                return
+ 
+       
+for i in read_file('11.txt'):
+    print(i.decode('gbk'))
+    
 
-     
+
+    
+    
+'''
 for i in fun(10):
     print(i,end = ' ')   
 #print(i for i in fun(10))
+'''
+        
 
-
-
-
+'''
+UDP包发送   飞秋协议
+udp=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  
+udp.connect(('192.168.1.12',2425))
+str =  '1_lbt4_10#32899#002481627512#0#0#0:1289671407:flyingzl:flyingzl:288:一日不见，如三月兮' 
+udp.send(str.encode(encoding='utf_8', errors='strict'))  
+'''
+    
+    
